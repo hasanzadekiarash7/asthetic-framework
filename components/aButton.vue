a6e0aeaac0e3cb5d73fc88af7d7f22798beec850
hasanzadekiarash7 <150239972+hasanzadekiarash7@users.noreply.github.com>
2024-05-17 01:46:36 -0700

    fix some problem

--- components/aButton.vue
+++ components/aButton.vue
@@ -1,70 +1,101 @@
 <template>
-    <button class="d-flex justify-content-space-around align-items-center radius-7"
-        :class="[ColorStateComputed, sizeComputed]" v-if="iconOnly">
-        <div>
+    <button class="flex justify-content-space-around align-items-center"
+        :class="[ColorStateComputed, sizeComputed, iconOnly ? 'radius-round space-p-a-2 space-m-a-2 ' : 'radius-6 space-m-a-2']">
+        <div v-if="!iconOnly">
             <Icon v-if="appendIconName" :name="appendIconName" :size="appendIconSize" />
             <slot name="appendIcon"></slot>
         </div>
-        <div>
+        <div v-if="!iconOnly">
             <p class="space-m-a-0">{{ label }}</p>
-            <slot name="label"></slot>
+            <slot></slot>
         </div>
-        <div>
+        <div v-if="!iconOnly">
             <Icon v-if="prependIconName" :name="prependIconName" :size="prependIconSize" />
             <slot name="prependIcon"></slot>
         </div>
-    </button>
-    <button v-else :class="[ColorStateComputed, sizeComputed]">
-        <Icon v-if="IconOnlyIconName" :name="IconOnlyIconName" :size="IconOnlyIconSize" />
-        <slot name="iconOnly"></slot>
+        <div v-if="iconOnly">
+            <Icon v-if="IconOnlyIconName" :class="[iconSize]" :name="IconOnlyIconName" :size="IconOnlyIconSize" />
+            <slot name="iconOnly"></slot>
+        </div>
     </button>
 </template>
 
 <script setup lang="ts">
 interface ButtonProps {
     label?: String,
-    appendIconName?: string,
+    appendIconName?: String,
     appendIconSize?: String,
-    IconOnlyIconName?: string,
+    IconOnlyIconName?: String,
     IconOnlyIconSize?: String,
     prependIconName?: String,
     prependIconSize?: String,
-    iconOnly?: Boolean,
+    iconOnly?: boolean,
     type?: String,
     size?: String
 }
 const props = defineProps<ButtonProps>()
+
 // ** computed
 const ColorStateComputed = computed(() => {
     switch (props.type) {
         case "primary":
-            return 'color-gray-50 bg-color-gray-700 primary-style';
+            return 'color-gray-50 bg-color-gray-700 primary-style outline-gray-200';
         case "secondary":
-            return 'color-gray-950 bg-color-gray-300 primary-style'
+            return 'color-gray-950 bg-color-gray-300 primary-style outline-gray-100';
         case "tertiary":
-            return 'color-gray-950 bg-color-gray-100 primary-style'
+            return 'color-gray-950 bg-color-gray-50 primary-style outline-gray-300';
         case "link":
-            return 'color-gray-950 bg-color-transparent primary-style'
-        case "Danger":
-            return 'color-red-50 bg-color-red-700 primary-style'
-        case "Success":
-            return 'color-green-50 bg-color-green-700 primary-style'
+            return 'color-gray-950 bg-color-transparent link-style';
+        case "danger":
+            return 'color-red-50 bg-color-red-500 primary-style outline-red-200';
+        case "success":
+            return 'color-green-50 bg-color-green-500 primary-style outline-green-200';
     }
 })
 const sizeComputed = computed(() => {
-    switch (props.size) {
-        case "xl":
-            return 'width-50'
-        case "lg":
-            return 'width-40'
-        case "md":
-            return 'width-30'
-        case "sm":
-            return 'width-20'
-        case "xs":
-            return 'width-10'
+    if (props.iconOnly) {
+        switch (props.size) {
+            case "xl":
+                return 'icon-width-x-large'
+            case "lg":
+                return 'icon-width-large'
+            case "md":
+                return 'icon-width-medium'
+            case "sm":
+                return 'icon-width-small'
+            case "xs":
+                return 'icon-width-x-small'
+        }
+    } else {
+        switch (props.size) {
+            case "xl":
+                return 'width-50 heading-x-small'
+            case "lg":
+                return 'width-40 label-large'
+            case "md":
+                return 'width-30 label-medium'
+            case "sm":
+                return 'width-20 label-small'
+            case "xs":
+                return 'width-10 label-x-small'
+        }
     }
 })
+const iconSize = computed(() => {
+    switch (props.size) {
+        case "xl":
+            return 'icon-x-large'
+        case "lg":
+            return 'icon-large'
+        case "md":
+            return 'icon-medium'
+        case "sm":
+            return 'icon-small'
+        case "xs":
+            return 'icon-x-small'
+    }
+})
+
 </script>
 
 <style lang="scss" scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,65 @@
 <template>
-    <div>
-        <!-- <UContainer> -->
-        <AButton type="primary" size="lg" label="hello" icon-only-name="mdi:eye" icon-only-size="24">
-            <template v-slot:label>
-
-            </template>
+    <div class="flex flex-flow justify-content-center align-items-center">
+        <AButton type="danger" icon-only size="xl" -icon-only-icon-name="line-md:downloading-loop">
         </AButton>
-        <!-- </UContainer> -->
+        <AButton type="success" icon-only size="lg" -icon-only-icon-name="line-md:downloading-loop">
+        </AButton>
+        <AButton type="primary" icon-only size="md" -icon-only-icon-name="line-md:downloading-loop">
+        </AButton>
+        <AButton type="secondary" icon-only size="sm" -icon-only-icon-name="line-md:downloading-loop">
+        </AButton>
+        <AButton type="tertiary" icon-only size="xs" -icon-only-icon-name="line-md:downloading-loop">
+        </AButton>
+        <AButton type="link" icon-only -icon-only-icon-name="line-md:downloading-loop">
+        </AButton>
+    </div>
+    <div class="flex flex-flow justify-content-center align-items-center">
+        <AButton type="danger" size="xl">
+            hello
+        </AButton>
+        <AButton type="success" size="lg">
+            hello
+        </AButton>
+        <AButton type="primary" size="md">
+            hello
+        </AButton>
+        <AButton type="secondary" size="sm">
+            hello
+        </AButton>
+        <AButton type="tertiary" size="xs">
+            hello
+        </AButton>
+        <AButton type="link">
+            hello
+        </AButton>
+    </div>
+    <div class="flex flex-flow justify-content-center align-items-center">
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="danger" size="xl" label="hello">
+        </AButton>
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="success" size="lg" label="hello">
+        </AButton>
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="primary" size="md" label="hello">
+        </AButton>
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="secondary" size="sm" label="hello">
+        </AButton>
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="tertiary" size="xs" label="hello">
+        </AButton>
+        <AButton append-icon-name="line-md:star-pulsating-twotone-loop" type="link" label="hello">
+        </AButton>
+    </div>
+    <div class="flex flex-flow justify-content-center align-items-center">
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="danger" size="xl" label="hello">
+        </AButton>
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="success" size="lg" label="hello">
+        </AButton>
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="primary" size="md" label="hello">
+        </AButton>
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="secondary" size="sm" label="hello">
+        </AButton>
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="tertiary" size="xs" label="hello">
+        </AButton>
+        <AButton prepend-icon-name="line-md:star-pulsating-twotone-loop" type="link" label="hello">
+        </AButton>
     </div>
 </template>
 
